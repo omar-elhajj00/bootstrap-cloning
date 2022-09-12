e9610ebc182979572a61c39351cfa7ec279dd41f
Omar El Hajj <omar.elhajj.00@gmail.com>
2022-09-12 20:31:07 +0300

    made the preventDefault Event to prevent refresh page when submit button is clicked

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
+//define the input by their ids
 let fullName = document.querySelector('#full-name');
 let email = document.querySelector('#email');
 let phoneNumber = document.querySelector('#phone-number');
 let message = document.getElementById('message-text');
+let sendForm = document.getElementById('send-form');
 const sendButton = document.getElementById('send-btn')
 
-console.log(fullName, email, phoneNumber, message,sendButton);
-sendButton.addEventListener('click', validateInfo);
+console.log(fullName, email, phoneNumber, message, sendButton, sendForm);
 
+sendForm.addEventListener("submit", function (evt) {
+    evt.preventDefault();
+  // do you ajax submission here
+    console.log('submitted')
+})
+
+// when click on send button
+sendButton.addEventListener('click', validateInfo);
 
 var lowerCaseLetters = /[a-z]/g;
 var upperCaseLetters = /[A-Z]/g;
@@ -14,9 +23,8 @@ var numbers = /[0-9]/g;
 
 
 function validateInfo(e) {
-    e.preventDefault();
     
-    console.log('haha');
+
 }
 
 
