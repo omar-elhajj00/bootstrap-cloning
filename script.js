1e4b0b52417e1949134ffd30d3b576caa64a9e6a
Omar El Hajj <omar.elhajj.00@gmail.com>
2022-09-12 22:57:52 +0300

    cleared the previous warnings in every new warningsince they were solved

--- script.js
+++ script.js
@@ -31,30 +31,46 @@ function validateInfo() {
     }
     // chec if there are 3 characters before @    
     else if (email.value.substring(0, email.value.indexOf('@')).length < 3) {
+        fullNAmeWarning.innerHTMl = null;
         emailWarning.innerHTML = 'Email should have minimum 3 characters before @';
+        // clear the previous warning
+        fullNAmeWarning.innerHTML = "";
+        
     // chec if there are 5 characters after @            
     }
     else if (email.value.substring(email.value.indexOf('@')+1,email.value.length).length < 5) {
         emailWarning.innerHTML = 'Email should have minimum 5 characters after @';
+        // clear the previous warning
+        fullNAmeWarning.innerHTML = "";
     }
     // check if the phone number start with +961
     else if (phoneNumber.value.substring(0,4) != "+961") {
         phoneNumberWarning.innerHTML = 'the phone number should start with +961';
+        // clear the previous warning
+        fullNAmeWarning.innerHTML = "";
+        emailWarning.innerHTML = "";
     }
     // check if there are 8 digits after +961 in phone number input
     else if (phoneNumber.value.substring(4,phoneNumber.value.length).length != 8) {
         phoneNumberWarning.innerHTML = 'the phone number should have 8 digits after +961';
+        // clear the previous warning
+        fullNAmeWarning.innerHTML = "";
+        emailWarning.innerHTML = "";
     }
     // check if the message have less than 100 characters
     else if (message.value.length<100) {
         messageWarning.innerHTML = 'message should have in minimum 100 characters';
+        // clear the previous warning
+        fullNAmeWarning.innerHTML = "";
+        emailWarning.innerHTML = "";
+        phoneNumberWarning.innerHTML = "";
     }
     //if everythig is okay
     else {
         successMessage.innerHTML = 'submitted , thank you !'; 
         // clear all the warnings
-        fullNAmeWarning.innerHTMl = "";
-        emailWarning = "";
+        fullNAmeWarning.innerHTML = "";
+        emailWarning.innerHTML = "";
         phoneNumberWarning.innerHTML = "";
         messageWarning.innerHTML = "";
     }
